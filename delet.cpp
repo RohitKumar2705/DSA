a5575f8a119a272cd55d999bac0b3ff12416cfd2
RohitKumar2705 <rk49305381@gmail.com>
2026-04-05 16:30:54 +0530

    Add files via upload

---
+++ delet.cpp
@@ -0,0 +1,134 @@
+
+#include<iostream>
+using namespace std;
+
+class node{
+
+    public:
+    int data ;
+    node*next;
+
+    node(int data){
+        this->data = data;
+        this->next = NULL;
+
+    }
+    ~node(){
+
+        int value = this->data;
+        if(this->next != NULL){
+            delete next;
+            this->next = NULL;
+
+        }
+        cout<<" memory is free for node with data "<<value <<endl;
+    }
+
+};
+
+
+   
+  void insertathead(node*&head,int d){
+    node*temp = new node(d);
+    temp->next = head;
+    head = temp;
+  }
+
+   void insertattail(node*tail,int d){
+    node*temp = new node(d);
+    tail->next = temp;
+    tail = tail->next;
+   }
+
+   void print(node*&head){
+    node*temp = head;
+
+    while(temp!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+
+    }
+     cout<<endl;
+   }
+
+    void insertatposition( node*&tail,node*&head,int position ,int d){
+
+        if(position == 1){
+            insertathead(head,d);
+            return;
+        }
+        node*temp = head;
+        int cnt = 1;
+
+        while(cnt<position - 1){
+            temp = temp->next;
+            cnt++;
+        }
+
+        if(temp->next == NULL){
+            insertattail(tail,d);
+            return ;
+        }
+        
+
+    
+
+    node*nodetoinsert = new node(d);
+    nodetoinsert -> next = temp->next;
+    temp->next = nodetoinsert;
+    }
+
+     void deletenode(int position,node*&head){
+        if(position==1){
+            node*temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+
+        }
+        else{
+          node*curr = head;
+          node*prev = NULL;
+
+          int cnt = 1;
+          while(cnt <= position ){
+            prev = curr ;
+            curr=  curr->next;
+             cnt++;
+          }
+            prev->next = curr->next;
+            curr-> next = NULL;
+                delete curr;
+        }
+     }
+
+int main(){
+    node*node1 = new node(10);
+    // cout<<node1->data<<endl;
+    // cout<<node1->next<<endl;
+    node*head = node1;
+    node*tail = node1;
+    print(head);
+
+    insertattail(tail,12);
+    print(head);
+
+    insertattail(tail,15);
+    print(head);
+     
+
+    // insertatposition(head,2,22);
+    // print(head);
+     
+    insertatposition( tail,head,2,22);
+    print(head);
+
+    cout<<"head"<<head->data<<endl;
+    cout<<"tail"<<tail->data<<endl;
+
+    // deletenode(1,head);
+    // print(head);
+
+    return 0;
+ 
+}
